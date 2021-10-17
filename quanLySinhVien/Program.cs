774a4863d9dfe86664ae26f16de81ae4b4e66ed5
your name <duc.d.62cntt@ntu.edu.vn>
2021-10-18 02:54:35 +0700

    add new file

---
+++ quanLySinhVien/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace dssv
+{
+
+    class SinhVien // details of student with simple input/output methods
+    {
+        string ten;
+        float dtb;
+
+        public void NhapSV()
+        {
+            Console.WriteLine("Nhap ho ten sinh vien");
+            string ten = Console.ReadLine();
+            Console.Write("Nhap diem trung binh: ");
+            dtb = float.Parse(Console.ReadLine());
+        }
+
+        public void XuatSV()
+        {
+            Console.WriteLine("Ten sinh vien: {0}", ten);
+            Console.WriteLine("Diem trung binh: {0}", dtb);
+            Console.WriteLine("\n--------------------------");
+        }
+    }
+
+    class Mang // một mảng các đối tượng sinh viên
+    {
+        SinhVien[] a;
+        int n;
+
+        public Mang() // hàm tạo
+        { a = new SinhVien[100]; }
+
+        public void NhapMang()
+        {
+            Console.WriteLine("Nhap so sv");
+            n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Nhap chi tiet sinh vien thu {0}", i + 1);
+                a[i] = new SinhVien();
+                a[i].NhapSV();
+            }
+        }
+
+        public void XuatMang()
+        {
+            for (int i = 0; i < n; i++) a[i].XuatSV();
+        }
+    }
+
+    class ChuongTrinh
+    {
+        static void Main(string[] args)
+        {
+            Mang a = new Mang();
+            a.NhapMang();
+            a.XuatMang();
+        }
+    }
+}
